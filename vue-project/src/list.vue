35ef31ce6b54b14480adaf1966aa420375a216c0
Nika Chigladze <nick.corlleone.05@gmail.com>
2023-01-25 02:27:22 +0400

    lesson

---
+++ vue-project/src/list.vue
@@ -0,0 +1,32 @@
+<template>
+ <div class="div1">
+   <ul>
+    <li v-for="item in list" :key="item.index">{{ item }}</li>
+   </ul>
+ </div>
+
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            list: ["Home","Product","Contact","AboutUs"]
+        }
+    }
+}
+
+</script>
+
+<style>
+ul {
+    display: flex ;
+} 
+
+li {
+    color: rgba(151, 33, 236, 0.874);
+  width: 100px;
+  font-weight: 600;
+}
+    
+</style>

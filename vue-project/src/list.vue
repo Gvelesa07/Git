a31abd8ca301ddbb21646747fc75c422b3d17309
Nika Chigladze <nick.corlleone.05@gmail.com>
2023-01-25 18:19:03 +0400

    bolo

--- vue-project/src/list.vue
+++ vue-project/src/list.vue
@@ -15,7 +15,7 @@
 
       <template>
         <ul>
-    <li v-for="item in menuList" :key="item.id">
+    <li v-for="item in menuData" :key="item.id">
      <a :href="item.url">{{ item.title }}</a>
      </li>
     </ul>
